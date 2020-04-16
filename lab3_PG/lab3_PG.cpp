e4ffd76d801b31aaea2f21c4d41f336163933635
LibrA <umashev58@gnail.com>
2020-04-16 16:33:07 +0300

    Немного изменений

--- lab3_PG/lab3_PG.cpp
+++ lab3_PG/lab3_PG.cpp
@@ -1,11 +1,13 @@
 ﻿#include <iostream>
 #include <string>
 
-int const LEN = 25;     //фактическая длина строки = LEN - 1 (последний символ - NULL)
+int const LEN = 26;     //фактическая длина строки = LEN - 1 (последний символ - NULL)
 
 using namespace std;
+int IVI();
 int compare(char LINE1[LEN], char LINE2[LEN], int n);
 void EnterIN(char LINE[LEN]);
+void ACTIONS_1(char LINE1[LEN], char LINE2[LEN], int N);
 
 int main() {
     int number;
@@ -13,19 +15,11 @@ int main() {
     cin >> number;
     while (number) {
         if (number == 1) {
-
-            int n;
-            cout << "Enter n less than " + to_string(LEN) + " - ";
-			cin >> n;
-			cin.clear();
-			cin.ignore();
-
+            int N = IVI();
             char LINE1[LEN], LINE2[LEN];
             EnterIN(LINE1);
             EnterIN(LINE2);
-            cout << LINE1 << endl;
-            cout << LINE2 << endl;
-			cout << compare(LINE1, LINE2, n) << endl;
+            ACTIONS_1(LINE1, LINE2, N);
         }
         if (number == 2) {}
         if (number == 3) {}
@@ -33,6 +27,19 @@ int main() {
         cin >> number;
     }
     return 0;
+}
+
+int IVI() {
+    int var;
+    cout << "Enter N less than " + to_string(LEN) + " - ";
+    cin >> var;
+    while (var >= LEN) {
+        cout << "ERROR.Enter N LESS than " + to_string(LEN) + " - ";
+        cin >> var;
+    }
+    cin.clear();
+    cin.ignore();
+    return var;
 }
 
 int compare(char LINE1[LEN], char LINE2[LEN], int n) {
@@ -44,7 +51,7 @@ int compare(char LINE1[LEN], char LINE2[LEN], int n) {
 }
 
 void EnterIN(char LINE[LEN]) {
-	cout << "Enter less than " + to_string(LEN) + " symbols" + ":";
+    cout << "Enter less than " + to_string(LEN) + " symbols" + ":";
 	cin.getline(LINE, 255);
 	int j = 0;
 	while (LINE[j] != NULL) j++;
@@ -54,4 +61,11 @@ void EnterIN(char LINE[LEN]) {
 		j = 0;
 		while (LINE[j] != NULL) j++;
 	}
+}
+
+void ACTIONS_1(char LINE1[LEN], char LINE2[LEN], int N) {
+    cout << LINE1 << " <=> ";
+    cout << LINE2 << endl;
+    cout << "Result of the comparsion - ";
+    cout << compare(LINE1, LINE2, N) << endl;
 }
