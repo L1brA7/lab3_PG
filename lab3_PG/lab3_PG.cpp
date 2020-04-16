b9c84ff6e85c22958bdafc813f131888c6fc1c1c
LibrA <umashev58@gnail.com>
2020-04-16 15:29:42 +0300

    Теперь точно готов.

--- lab3_PG/lab3_PG.cpp
+++ lab3_PG/lab3_PG.cpp
@@ -1,11 +1,13 @@
 ﻿#include <iostream>
 #include <string>
 
-using namespace std;
-int compare(char LINES, int n);
 
 int const LINES_Q = 2; //количество строк
-int const LEN = 6;     //фактическая длина строки = LEN - 1 (последний символ - NULL)
+int const LEN = 6; //фактическая длина строки = LEN - 1 (последний символ - NULL)
+
+using namespace std;
+int compare(char LINES, int n);
+void create(char LINES[LINES_Q][LEN]);
 
 int main() {
     int number;
@@ -16,22 +18,16 @@ int main() {
 
             int n;
             //cout << "Enter n less than " + to_string(LEN) + " - ";
-            //cin >> n;
+			//cin >> n;
 
 			cin.clear();
 			cin.ignore();
 
 			char LINES[LINES_Q][LEN];
-			for (int i = 0; i < LINES_Q; i++) {
-				cout << "Enter less than " + to_string(LEN) + " symbols" + ":" + "\n";
-				cin.getline(LINES[i], LEN);
-				while (LINES[i][LEN - 1] != NULL || LINES[i][LEN - 1] != '\0') {
-					cout << "ERROR.Enter LESS than " + to_string(LEN) + " symbols" + ":";
-					cin.getline(LINES[i], LEN);
-				}
-			}
+			create(LINES);
 			cout << LINES[0] << endl;
 			cout << LINES[1] << endl;
+
         }
         if (number == 2) {}
         if (number == 3) {}
@@ -48,4 +44,19 @@ int compare(char LINES, int n) {
         }
     }
     return 0;
+}
+
+void create(char LINES[LINES_Q][LEN]) {
+	for (int i = 0; i < LINES_Q; i++) {
+		cout << "Enter less than " + to_string(LEN) + " symbols" + ":" + "\n";
+		cin.getline(LINES[i], 255);
+		int j = 0;
+		while (LINES[i][j] != NULL) j++;
+		while (j >= LEN) {
+			cout << "ERROR.Enter LESS than " + to_string(LEN) + " symbols" + ":";
+			cin.getline(LINES[i], 255);
+			j = 0;
+			while (LINES[i][j] != NULL) j++;
+		}
+	}
 }
