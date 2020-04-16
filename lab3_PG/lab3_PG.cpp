41129db82f48297df9cd09f765a01740965e56b8
LibrA <umashev58@gnail.com>
2020-04-16 20:33:34 +0300

    Задание 2 готово.

--- lab3_PG/lab3_PG.cpp
+++ lab3_PG/lab3_PG.cpp
@@ -1,13 +1,28 @@
-﻿#include <iostream>
+﻿#pragma warning(disable : 4996)
+#include <iostream>
 #include <string>
-
-int const LEN = 26;     //фактическая длина строки = LEN - 1 (последний символ - NULL)
+#include <fstream>
+#include <cstring>
 
 using namespace std;
-int IVI();
-int compare(char LINE1[LEN], char LINE2[LEN], int n);
+
+//Задание 1:
+
+int const LEN = 26;
+//Ввод N c проверкой по отношению к LEN
+int N_INPUT();
+//Сравнение первых n символов двух строк
+int COMPARE(char LINE1[LEN], char LINE2[LEN], int n);
+//Ввод строки (с ограничителем)
 void EnterIN(char LINE[LEN]);
+//действия для задания 1
 void ACTIONS_1(char LINE1[LEN], char LINE2[LEN], int N);
+
+//Задание 2
+void task2(ifstream& TEXT);
+
+//Задание 3
+void task3(ifstream &TEXT);
 
 int main() {
     int number;
@@ -15,21 +30,28 @@ int main() {
     cin >> number;
     while (number) {
         if (number == 1) {
-            int N = IVI();
+            int N = N_INPUT();
             char LINE1[LEN], LINE2[LEN];
             EnterIN(LINE1);
             EnterIN(LINE2);
             ACTIONS_1(LINE1, LINE2, N);
         }
-        if (number == 2) {}
-        if (number == 3) {}
+        if (number == 2) {
+			ifstream TEXT;
+            TEXT.open("text.txt");
+            task2(TEXT);
+            TEXT.close();
+        }
+        if (number == 3) {
+
+        }
         cout << "Enter the number, or enter 0 to close - ";
         cin >> number;
     }
     return 0;
 }
 
-int IVI() {
+int N_INPUT() {
     int var;
     cout << "Enter N less than " + to_string(LEN) + " - ";
     cin >> var;
@@ -42,7 +64,7 @@ int IVI() {
     return var;
 }
 
-int compare(char LINE1[LEN], char LINE2[LEN], int n) {
+int COMPARE(char LINE1[LEN], char LINE2[LEN], int n) {
     for (int i = 0; i < n; i++) {
         if (LINE1[i] > LINE2[i]) return 1;
         if (LINE1[i] < LINE2[i]) return -1;
@@ -67,5 +89,22 @@ void ACTIONS_1(char LINE1[LEN], char LINE2[LEN], int N) {
     cout << LINE1 << " <=> ";
     cout << LINE2 << endl;
     cout << "Result of the comparsion - ";
-    cout << compare(LINE1, LINE2, N) << endl;
+    cout << COMPARE(LINE1, LINE2, N) << endl;
+}
+
+void task2(ifstream& TEXT) {
+    char line[255];
+    char *word;
+	char delim[] = " !?.,\n";
+    while (TEXT.getline(line, 255, NULL)) {
+        word = strtok(line, delim);
+		while (word != NULL) {
+			if(strlen(word) > 3) cout << word << endl;
+			word = strtok(NULL, delim);
+		}
+    }
+}
+
+void task3(ifstream &TEXT) {
+    
 }
